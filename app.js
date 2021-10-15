a68ce20e2b8685188f300cca5f43cfa303bd564b
gabrielraymond <gabrielraymond.grd@gmail.com>
2021-10-15 21:09:50 +0800

    change mongoose version

--- app.js
+++ app.js
@@ -26,7 +26,12 @@ const MongoDBStore = require("connect-mongo")(session);
 // const dbUrl = process.env.DB_URL
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp2';
 
-mongoose.connect(dbUrl);
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
 
 const db = mongoose.connection;
